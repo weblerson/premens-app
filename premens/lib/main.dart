2fb3ac0a53c7f373e565774e3e0addcc756241be
weblerson <zonejr4@gmail.com>
2022-10-16 17:30:09 -0400

    control buttons now have functions

--- premens/lib/main.dart
+++ premens/lib/main.dart
@@ -5,9 +5,15 @@ import 'package:premens/app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+import 'package:flutter/services.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
 
   runApp(const App());
 }
--- premens/lib/screens/controls.dart
+++ premens/lib/screens/controls.dart
@@ -4,6 +4,9 @@ import 'package:premens/components/controls/controls_bg.dart';
 import 'package:premens/components/controls/buttons.dart';
 import 'package:premens/components/general/vertical_padding.dart';
 
+import 'package:premens/data/lerson_back.dart';
+import 'package:premens/data/auth_data.dart';
+
 class ControlsScreen extends StatelessWidget {
   const ControlsScreen({super.key});
 
@@ -18,14 +21,154 @@ class ControlsScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              ControlButton(
-                  imagePath: 'assets/images/arrow_up.png', text: 'Subir'),
+              GestureDetector(
+                onLongPress: () async {
+                  Map<String, dynamic> data = {
+                    'id': AuthData.instance.uniqueId,
+                    'username': AuthData.instance.user,
+                    'token': AuthData.instance.token,
+                    'key': 'up',
+                    'value': true
+                  };
+
+                  final res = await LersonBack.instance.control(data);
+                  if (res['success']) {
+                    SnackBar snackBar =
+                        const SnackBar(content: Text('Subindo...'));
+
+                    // ignore: use_build_context_synchronously
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    return;
+                  }
+
+                  SnackBar snackBar = SnackBar(content: Text(res['body']));
+
+                  // ignore: use_build_context_synchronously
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                  return;
+                },
+                onLongPressUp: () async {
+                  Map<String, dynamic> data = {
+                    'id': AuthData.instance.uniqueId,
+                    'username': AuthData.instance.user,
+                    'token': AuthData.instance.token,
+                    'key': 'up',
+                    'value': false
+                  };
+
+                  final res = await LersonBack.instance.control(data);
+                  if (res['success']) {
+                    SnackBar snackBar = const SnackBar(content: Text('Parou!'));
+
+                    // ignore: use_build_context_synchronously
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    return;
+                  }
+
+                  SnackBar snackBar = SnackBar(content: Text(res['body']));
+
+                  // ignore: use_build_context_synchronously
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                  return;
+                },
+                child: ControlButton(
+                    imagePath: 'assets/images/arrow_up.png', text: 'Subir'),
+              ),
               const VerticalPadding(height: 30.0),
-              ControlButton(
-                  imagePath: 'assets/images/bi_arrow.png', text: 'Automático'),
+              GestureDetector(
+                onLongPress: () async {
+                  Map<String, dynamic> data = {
+                    'id': AuthData.instance.uniqueId,
+                    'username': AuthData.instance.user,
+                    'token': AuthData.instance.token,
+                    'key': 'automatico',
+                    'value': true
+                  };
+
+                  final res = await LersonBack.instance.control(data);
+                  if (res['success']) {
+                    SnackBar snackBar = const SnackBar(
+                        content: Text('Modo Automático Ativado!'));
+
+                    // ignore: use_build_context_synchronously
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    return;
+                  }
+
+                  SnackBar snackBar = SnackBar(content: Text(res['body']));
+
+                  // ignore: use_build_context_synchronously
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                  return;
+                },
+                child: ControlButton(
+                    imagePath: 'assets/images/bi_arrow.png',
+                    text: 'Automático'),
+              ),
               const VerticalPadding(height: 30.0),
-              ControlButton(
-                  imagePath: 'assets/images/arrow_down.png', text: 'Descer')
+              GestureDetector(
+                onLongPress: () async {
+                  Map<String, dynamic> data = {
+                    'id': AuthData.instance.uniqueId,
+                    'username': AuthData.instance.user,
+                    'token': AuthData.instance.token,
+                    'key': 'down',
+                    'value': true
+                  };
+
+                  final res = await LersonBack.instance.control(data);
+                  if (res['success']) {
+                    SnackBar snackBar =
+                        const SnackBar(content: Text('Descendo...'));
+
+                    // ignore: use_build_context_synchronously
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    return;
+                  }
+
+                  SnackBar snackBar = SnackBar(content: Text(res['body']));
+
+                  // ignore: use_build_context_synchronously
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                  return;
+                },
+                onLongPressUp: () async {
+                  Map<String, dynamic> data = {
+                    'id': AuthData.instance.uniqueId,
+                    'username': AuthData.instance.user,
+                    'token': AuthData.instance.token,
+                    'key': 'down',
+                    'value': false
+                  };
+
+                  final res = await LersonBack.instance.control(data);
+                  if (res['success']) {
+                    SnackBar snackBar = const SnackBar(content: Text('Parou!'));
+
+                    // ignore: use_build_context_synchronously
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    return;
+                  }
+
+                  SnackBar snackBar = SnackBar(content: Text(res['body']));
+
+                  // ignore: use_build_context_synchronously
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                  return;
+                },
+                child: ControlButton(
+                    imagePath: 'assets/images/arrow_down.png', text: 'Descer'),
+              )
             ],
           ),
         )
