50c499f448a7379674d212bd03bac5ceacfccc5d
weblerson <zonejr4@gmail.com>
2022-10-16 16:46:03 -0400

    added http post request to control route

--- premens/lib/data/lerson_back.dart
+++ premens/lib/data/lerson_back.dart
@@ -2,16 +2,17 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class LersonBack {
+  Map<String, String> headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+
   static LersonBack instance = LersonBack();
 
   String link = 'https://9hegx3.deta.dev';
 
   Future<Map<String, dynamic>> auth(Map<String, String> data) async {
     Uri url = Uri.parse('$link/auth');
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    };
 
     String body = jsonEncode(data);
     final res = await http.post(url, headers: headers, body: body);
@@ -23,10 +24,17 @@ class LersonBack {
 
   Future<Map<String, dynamic>> register(Map<String, String> data) async {
     Uri url = Uri.parse('$link/register');
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    };
+
+    String body = jsonEncode(data);
+    final res = await http.post(url, headers: headers, body: body);
+
+    return res.statusCode == 200
+        ? jsonDecode(utf8.decode(res.bodyBytes))
+        : {'success': false, 'body': 'Ocorreu um erro no sistema.'};
+  }
+
+  Future<Map<String, dynamic>> control(Map<String, dynamic> data) async {
+    Uri url = Uri.parse('$link/control');
 
     String body = jsonEncode(data);
     final res = await http.post(url, headers: headers, body: body);
